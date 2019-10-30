7e55573db4d631e5b50a809ab9ea6b52123d2b93
Aries <aries.kivitoz@gmail.com>
2019-10-30 17:59:26 -0600

    temas de javascript

---
+++ javascript/objetos.js
@@ -0,0 +1,31 @@
+/* var persona = {
+    nombre: 'aries',
+    edad: 27,
+    casado: false,
+    hijos: {
+        nombre: 'christopher',
+        edad: 3
+    }
+}
+
+console.log(persona.hijos); */
+
+
+var arrayObjetos = [
+    {
+        nombre: 'Aries',
+        edad: 27
+    },
+    {
+        nombre: 'Jose',
+        edad: 24
+    },
+    {
+        nombre: 'Antonio',
+        edad: 15
+    }
+]
+
+for( let i of arrayObjetos){
+    console.log(i);
+}
